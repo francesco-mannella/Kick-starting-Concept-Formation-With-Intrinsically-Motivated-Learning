21afc005f5df7f121733c9e37c37e907390cfad9
Julian Zubek <zubekj@gmail.com>
2023-12-13 15:29:59 +0100

    SMPredict converted to PyTorch

--- src/SMPredict.py
+++ src/SMPredict.py
@@ -1,75 +1,63 @@
 import numpy as np
-import tensorflow as tf
-import tensorflow.keras as keras
-import params
+import torch
 
-def out_fun(x):
-    return 1/(1 + tf.exp(-x))
 
 class SMPredict:
 
-    def __init__(self, inp_num, out_num, lr=0.1, name="predict"): 
+    def __init__(self, inp_num, out_num, lr=0.1):
         self.inp_num = inp_num
         self.out_num = out_num
         self.lr = lr
-        self.name = name
-        initializer = tf.keras.initializers.GlorotUniform()
 
         # Setting the model
-        self.inp_layer = keras.layers.Input(shape=(self.inp_num),name=f"{self.name}_input")
-        self.out_layer = keras.layers.Dense(self.out_num, 
-                name=f"{self.name}_layer",activation="sigmoid", 
-            kernel_initializer=keras.initializers.zeros(),
-            bias_initializer=keras.initializers.zeros()) 
-
-        self.model = keras.Model(inputs=self.inp_layer, outputs=self.out_layer(self.inp_layer), name=self.name)
-        self.model.compile(optimizer=keras.optimizers.Adam(learning_rate=self.lr),
-                loss="binary_crossentropy", metrics=None)
-        self.model.build([None, self.inp_num])
-        # self.summary = self.model.summary()
+        self.model = torch.nn.Linear(self.inp_num, self.out_num)
+        torch.nn.init.xavier_uniform(self.model.weight)
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
+        self.loss = torch.nn.BCEWithLogitsLoss()
 
         self.t = 0
 
     def update(self, patterns, labels):
-        patterns = tf.cast(patterns, dtype="float32")
-        self.loss = self.model.train_on_batch(patterns, labels)
-        return self.loss
+        self.optimizer.zero_grad()
+        output = self.model(torch.tensor(patterns).float())
+        loss = self.loss(output, torch.tensor(labels).unsqueeze(-1))
+        loss.backward()
+        self.optimizer.step()
+        return loss
 
-    def weights(self):
-        #return self.out_layer.get_weights()[0]
-        return self.out_layer.get_weights()
+    def get_weights(self):
+        # return self.out_layer.get_weights()[0]
+        return self.model.weight
 
     def set_weights(self, weights):
-        #keras.backend.set_value(self.model.get_layer(f"{self.name}_layer",).kernel, weights)
-        self.model.get_layer(f"{self.name}_layer",).set_weights(weights)
+        with torch.no_grad():
+            self.model.copy_(weights)
 
     def spread(self, inp):
         assert len(inp.shape) == 2
-        inp = tf.cast(inp, dtype="float32")
-        comp = self.model.predict(inp, verbose=0)
+        comp = self.model(torch.tensor(inp).float()).detach().numpy()
         comp = 2*np.maximum(0, comp - 0.5)
         return comp
 
-if __name__ == "__main__":
 
-    import matplotlib.pyplot as plt
+if __name__ == "__main__":
 
     inp_num = 2
     out_num = 1
     patterns_size = 10000
     epochs = 150
-    
+
     labels = np.zeros(patterns_size)
     labels[patterns_size//2:] = 1
     patterns = np.vstack([labels, 1-labels]).T \
             + 0.01*np.random.randn(patterns_size, 2)
     predict = SMPredict(inp_num, out_num)
-   
+
     idcs = np.arange(patterns_size)
     for t in range(epochs):
         np.random.shuffle(idcs)
         preds = predict.spread(patterns)
         loss = predict.update(patterns[idcs], labels[idcs])
-        
+
         comp = 2*np.abs(preds - 0.5)
         print(loss, comp.mean())
