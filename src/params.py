Fix: return to proper match calculation in controller

src/params.py
@@ -36,12 +36,12 @@ policy_base = np.pi*0.25
 explore_sigma = 8
 
 # Modalities order: visual, touch, proprioception, action
-#modalities_weights = [1., 1., 1., 1.]
-modalities_weights = [0., 1., 0., 0.]  # select touch only
-match_th = 0.1
+modalities_weights = [1., 1., 1., 1.]
+#modalities_weights = [0., 1., 0., 0.]  # select touch only
+match_th = 0.2
 match_incr_th = 0.05
 cum_match_stop_th = 15.0
-cum_match_success_th = 3.0
+cum_match_success_th = 2.0
 predict_lr = 0.005
 reach_grip_prop = 0.1
 
@@ -49,7 +49,7 @@ predict_ampl = 2
 predict_base_ampl = 2
 predict_ampl_prop = 0.95
 
-epochs = 5
+epochs = 400
 pretest_epochs = -1
 batch_size = 24
 tests = 12