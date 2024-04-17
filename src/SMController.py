Fixed a bug in policy scaling (explore_sigma)

src/SMController.py
@@ -132,7 +132,8 @@ class SMController:
         comp = SMController.comp_fun(rcomp)
         self.policy_noise = self.rng.randn(*policies.shape)
 
-        policies = self.explore_sigma * (policies + (1 - comp) * self.policy_noise)
+        #policies = self.explore_sigma * (policies + (1 - comp) * self.policy_noise)
+        policies = (policies + (1 - comp) * self.policy_noise)
         return policies, comp, rcomp
 
     def computeMatchSimple(self, v_p, ss_p, p_p, a_p, g_p):
@@ -256,7 +257,10 @@ class SMController:
             idcs = mch_idcs
 
             modulate = cgoals[idcs] * matches[idcs]
-            modulate /= modulate.sum() # Normalize modulation
+            #modulate /= modulate.sum() # Normalize modulation
+
+            #modulate *= 0 # TEST: No learning!!!
+
             mean_modulation = modulate.mean()
             
             # update maps
@@ -284,8 +288,11 @@ class SMController:
             #self.predict.update(goals[idcs], matches[idcs] > th)
             # update predictor: success is defined by cumulative match increment
             th = params.cum_match_success_th * competences[idcs]
+            #th = params.cum_match_success_th # TEST: no competence threshold modulation
             n_matches = mch_idcs.reshape((params.batch_size, -1)).sum(axis=-1)[:, None]
-            self.predict.update(goals[idcs], n_matches > th)
+            #th = np.median(n_matches) # TEST: fixed success at 0.5
+            print((n_matches >= th).mean())
+            self.predict.update(goals[idcs], n_matches >= th)
 
         elif pretest:
             if not hasattr(self, "count"):

src/params.py
@@ -30,8 +30,8 @@ base_match_sigma = 1
 match_sigma = 3
 base_internal_sigma = 1
 internal_sigma = 3
-base_lr = 0.01
-stm_lr = 0.05
+base_lr = 0.02
+stm_lr = 0.2
 policy_base = np.pi*0.25
 explore_sigma = 8
 
@@ -41,7 +41,7 @@ modalities_weights = [1., 1., 1., 1.]
 match_th = 0.1
 match_incr_th = 0.05
 cum_match_stop_th = 10.0
-cum_match_success_th = 2.0
+cum_match_success_th = 4.0
 predict_lr = 0.005
 reach_grip_prop = 0.1
 
@@ -49,7 +49,7 @@ predict_ampl = 2
 predict_base_ampl = 2
 predict_ampl_prop = 0.95
 
-epochs = 10
+epochs = 800
 pretest_epochs = -1
 batch_size = 24
 tests = 12