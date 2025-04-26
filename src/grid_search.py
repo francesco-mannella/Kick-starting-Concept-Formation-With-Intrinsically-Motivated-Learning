8a5ba5f46c0802dd9ca7556d0f97f3cdcab1149f
Francesco Mannella <francesco.mannella@gmail.com>
2025-04-26 23:28:10 +0200

    tests on match andi representationw

--- src/grid_search.py
+++ src/grid_search.py
@@ -48,10 +48,13 @@ def optimize_option_key(options_str):
 
 
 params = {
-    "cum_match_stop_th": [3.0],
+    "cum_match_stop_th": [8.0],
+    "representation_sigma": [2],
+    "base_match_sigma": [8],
+    "match_sigma": [8],
     "predict_rl": [0.02],
-    "obj_fix_prob": [0.6],
-    "obj_var_prob": [1.5, 2.0],
+    "obj_fix_prob": [0.8],
+    "obj_var_prob": [0.7, 1.2],
     "obj_x": [2],
     "obj_y": [2],
     "epochs": [400],
