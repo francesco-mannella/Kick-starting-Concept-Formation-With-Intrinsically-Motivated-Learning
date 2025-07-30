8d44702b7527de4a7b4a54918aa8746b812fc6b4
Julian Zubek <zubekj@gmail.com>
2025-07-30 17:48:46 +0200

    Reverted to local touch match increase

--- src/SMMain.py
+++ src/SMMain.py
@@ -313,8 +313,9 @@ class Main:
 
                     # ####### Dataset Filtering 
                     # Select time steps when internal representation of touch modality changes
-                    mmask = (ss_p[:, i] != ss_p[:, i-1]).any(axis=-1)
-                    #mmask = match_value_per_mod[:, i, 1] > match_value_per_mod[:, i-1, 1]
+                    #mmask = (ss_p[:, i] != ss_p[:, i-1]).any(axis=-1)
+                    # Select time steps when match in touch modality increases
+                    mmask = match_value_per_mod[:, i, 1] > match_value_per_mod[:, i-1, 1]
 
                     # Select only timesteps where match increases *globally* over a certain threshold
                     mmask[match_value[:, i] - max_match[:, i] < params.match_incr_th] = 0
