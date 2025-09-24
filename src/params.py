8b9b96e39566e9a472a9e68aaab9bd8ee2dec831
Francesco Mannella <francesco.mannella@gmail.com>
2025-09-24 16:17:06 +0200

    Reset position params

--- src/params.py
+++ src/params.py
@@ -64,8 +64,8 @@ class Parameters(ParameterManager):
         obj_fix_prob=2.0,
         obj_var_prob=0.6,
         obj_rot_var=0.5 * 3.1415922653,
-        obj_x=5,
-        obj_y=4,
+        obj_x=2,
+        obj_y=0.25,
         maximum_goal_activation=575.0,
     ):
 
