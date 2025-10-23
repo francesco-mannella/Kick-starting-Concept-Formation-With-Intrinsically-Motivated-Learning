4d8ec09ee3061c84a6fb0f49364eaabe1ad9ef24
Francesco Mannella <francesco.mannella@gmail.com>
2025-10-23 10:01:52 +0200

    policy weights

--- src/wplot.py
+++ src/wplot.py
@@ -1,24 +1,10 @@
-import argparse
-
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-parser = argparse.ArgumentParser(description="Process some directories.")
-parser.add_argument(
-    "--data_dir", type=str, required=True, help="Path to data directory"
-)
-args = parser.parse_args()
-
-data_dir = args.data_dir
-
 # Load actuator maps and weight data
-actuator_maps = np.load(
-    f"{data_dir}/www/StoredGripActuatorMap.npy", allow_pickle=True
-)
-weight_data = np.load(
-    f"{data_dir}/storage/000999/weights.npy", allow_pickle=True
-)[0]
+actuator_maps = np.load("www/StoredGripActuatorMap.npy", allow_pickle=True)
+weight_data = np.load("storage/000999/weights.npy", allow_pickle=True)[0]
 
 # Define constants
 num_touch_sensors = 40
@@ -34,21 +20,25 @@ fig, axes = plt.subplots(1, 3, figsize=(10, 4))
 def plot_touch_map(actuator_maps, num_touch_sensors, grid_size, grid_side, ax):
     """Plot the touch map on the given axis."""
     touch_map = actuator_maps[:num_touch_sensors, :grid_size]
-    touch_map = touch_map.reshape(10, 4, grid_side, grid_side)
-    touch_map = touch_map.transpose(2, 0, 3, 1).reshape(
+    touch_map = touch_map.reshape(4, 10, grid_side, grid_side)
+    touch_map = touch_map.transpose(2, 1, 3, 0).reshape(
         grid_side * 10, grid_side * 4
     )
     ax.imshow(touch_map, aspect=2 / 5)
-    yticks = np.linspace(4.5, 44.5, 5)
-    xticks = np.linspace(2, 17, 5)
-    ax.set_yticks(yticks, np.arange(5))
-    ax.set_xticks(xticks, np.arange(5))
-    for x in np.linspace(0, 19, 6)[:-1]:
-        ax.plot([x, x], [0, 49], c="white")
-    for y in np.linspace(0, 49, 6)[1:]:
-        ax.plot([0, 20], [y, y], c="white")
-    ax.set_xlim(0, 19)
-    ax.set_ylim(0, 49)
+    # yticks = np.linspace(4.5, 44.5, 5)
+    # xticks = np.linspace(2, 17, 5)
+    # ax.set_yticks(yticks, np.arange(5))
+    # ax.set_xticks(xticks, np.arange(5))
+    for x in np.linspace(-0.5, 19.5, 6)[1:-1]:
+        ax.plot([x, x], [-0.5, 49.5], c="white")
+    for x in np.linspace(-0.5, 19.5, 21)[1:-1]:
+        ax.plot([x, x], [0, 49.5], lw=0.2,c="white")
+    for y in np.linspace(-0.5, 49.5, 6)[1:-1]:
+        ax.plot([-0.5, 19.5], [y, y], c="white")
+    for y in np.linspace(-0.5, 49.5, 51)[1:-1]:
+        ax.plot([-0.5, 19.5], [y, y], lw=0.2,c="white")
+    # ax.set_xlim(0, 19)
+    # ax.set_ylim(0, 49)
 
 
 def plot_posture_maps(
@@ -167,30 +157,31 @@ plot_eyepos_maps(
     axes[2],
 )
 
-# %%
-for joint in range(5):
-    fig, axes = plt.subplots(11, 11, figsize=(8, 3))
 
-    for i, ax in enumerate(axes.flatten()):
-        ax.set_axis_off()
-        row, col = i // 11, i % 11
-        p = (row - 1) * 10 + (col - 1)
-        if col > 0 and row > 0:
-            plot_weights(
-                weight_data,
-                grid_size,
-                grid_side,
-                prototype=p,
-                joint=joint,
-                ax=ax,
-            )
-        else:
-            if col == 0 and row != 0:
-                ax.text(0.5, 0.5, f"{row}")
-            elif row == 0 and col != 0:
-                ax.text(0.5, 0.5, f"{col}")
-    fig.suptitle(f"Joint n.{joint}")
-    fig.tight_layout(pad=0.1)
+# for joint in range(5):
+#     fig, axes = plt.subplots(11, 11, figsize=(8, 3))
+# 
+#     for i, ax in enumerate(axes.flatten()):
+#         ax.set_axis_off()
+#         row, col = i // 11, i % 11
+#         p = (row - 1) * 10 + (col - 1)
+#         if col > 0 and row > 0:
+#             plot_weights(
+#                 weight_data,
+#                 grid_size,
+#                 grid_side,
+#                 prototype=p,
+#                 joint=joint,
+#                 ax=ax,
+#             )
+#         else:
+#             if col == 0 and row != 0:
+#                 ax.text(0.5, 0.5, f"{row}")
+#             elif row == 0 and col != 0:
+#                 ax.text(0.5, 0.5, f"{col}")
+#     fig.suptitle(f"Joint n.{joint}")
+#     fig.text(0.1, 0.1, "1")
+#     fig.tight_layout(pad=0.1)
 
 # Display the plots
 plt.show()
